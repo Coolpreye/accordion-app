f38d799d2caf94ca779dcf7eea9cc0312e1d3e22
Coolpreye <kerebipreye@gmail.com>
2020-11-29 16:24:21 -0800

    adjusted the width of the accordions

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <React.Fragment>
       <div className="mt-20 mb-10 p-8 shadow-lg mx-auto max-w-3xl w-11/12 bg-white rounded">
                 <div className="md:flex md:justify-between">
-                    <h2 className="text-sm sm:text-xl  tracking-wider md:mt-2">Questions And Answers About Login</h2>
+                    <h2 className="text-sm sm:text-xl md:text-3xl w-48 tracking-wider md:mt-2">Questions And Answers About Login</h2>
                     <div className="flex flex-col">
                     {
                         questions.map(question => {
--- src/QuestionsComponents.js
+++ src/QuestionsComponents.js
@@ -5,13 +5,13 @@ export const QuestionsComponents = ({ title, info }) => {
 
     return (
         <React.Fragment>
-                <div className="mt-3 shadow-lg rounded px-6 py-3 border border-gray-200">
+                <div className="mt-3 shadow-lg rounded px-6 py-3 border border-gray-200 max-w-md">
                     <div className="">
                         <div className="flex justify-between">
-                            <h4 className="text-xs md:text-sm md:font-semibold">{title}</h4>
+                            <h4 className="text-xs md:text-lg md:font-semibold">{title}</h4>
             <span className="bg-clr3 text-clr2 rounded-full btn px-2 self-start pb-1 text-sm sm:text-xl cursor-pointer" onClick={()=> setShowInfo(!showInfo)}>{showInfo ? "-" : "+"}</span>
                         </div>
-                        { showInfo && <p className="mt-4 text-xs md:text-sm text-gray-500">{info}</p>}
+                        { showInfo && <p className="mt-4 text-xs md:text-lg text-gray-500 w">{info}</p>}
                     </div>
                 </div>
         </React.Fragment>
